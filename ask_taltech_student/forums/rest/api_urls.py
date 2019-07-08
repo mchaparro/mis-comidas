a9984771dca3ccf99002469928da14810563da17
Miguel Chaparro <miguel@thorgate.eu>
2019-07-08 14:37:40 +0300

    Remove unused code from ask a student project Create Recipes models

--- ask_taltech_student/forums/rest/api_urls.py
+++
@@ -1,9 +0,0 @@
-from rest_framework.routers import SimpleRouter
-
-from forums.rest.views import ForumViewSet
-
-
-router = SimpleRouter(trailing_slash=False)
-router.register(r'forums', ForumViewSet)
-
-urlpatterns = router.urls
